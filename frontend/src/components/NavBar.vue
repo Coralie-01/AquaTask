7ba462abf3d604e5ad311ef490240470e335d89f
SERRAND Coralie <coralie.serrand@telecomnancy.eu>
2023-07-03 15:58:41 +0900

    [+] NavBar component

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="navbar bg-base-100">
+    <div class="navbar bg-secondary text-neutral-content">
   <div class="flex-1">
-    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <img src="/src/assets/logo.png" style="width: 6.80806rem;
+height: 3.6875rem;
+flex-shrink: 0;" />
+    <a class="btn btn-ghost normal-case text-xl">AquaTask</a>
   </div>
   <div class="flex-none">
     <div class="dropdown dropdown-end">
       <label tabindex="0" class="btn btn-ghost btn-circle avatar">
         <div class="w-10 rounded-full">
-          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          <i class="fi fi-rr-user"></i>
         </div>
       </label>
       <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
@@ -24,3 +27,10 @@
   </div>
 </div>
 </template>
+
+<style>
+.fi {
+  color: white;
+  font-size: 2.5rem;
+}
+</style>
