a64966c19148fac2d8b45fcf1bd9bde7136245f4
SERRAND Coralie <coralie.serrand@telecomnancy.eu>
2023-07-04 11:24:08 +0900

    [+] update login

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -4,18 +4,15 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore('user', {
   state: () => ({
     loggedIn: false,
-    user_id: null
   }),
   actions: {
-    login(userID) {
+    login() {
       // Implement your login logic here
       this.loggedIn = true
-      this.user_id = userID
     },
     logout() {
       // Implement your logout logic here
       this.loggedIn = false
-      this.userId = null
     }
   }
 })
