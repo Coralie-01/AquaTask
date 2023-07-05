ad4a2933c42094a3cee1a603ae47affb75cfc49f
SERRAND Coralie <coralie.serrand@telecomnancy.eu>
2023-07-05 20:11:18 +0900

    [+] aquarium time progress while session running, + fish animation

--- frontend/src/components/CountDown.vue
+++ frontend/src/components/CountDown.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex flex-col items-center gap-32">
-    <div v-if="!sessionStarted" class="relative rounded-full w-96 h-96">
+    <div class="relative rounded-full w-96 h-96">
       <div class="aquarium absolute bottom-0 w-full rounded-full h-full"></div>
+      <div class="water absolute bottom-0 w-full rounded-full h-full"></div>
       <!--div class="absolute top-0 bg-base-300 w-full h-1/5 rounded-t-full"></div-->
       <round-slider
+        v-if="!sessionStarted"
         v-model="totalMinutes"
         start-angle="132"
         end-angle="+275"
@@ -20,15 +22,34 @@
         showTooltip="false"
         class="absolute inset-0 left-1/2 transform -translate-x-1/2 top-1/2 transform -translate-y-1/2"
       ></round-slider>
+      <round-slider
+        v-if="sessionStarted"
+        v-model="remainingTime"
+        start-angle="132"
+        end-angle="+275"
+        min = "0"
+        max = "7200"
+        line-cap="round"
+        radius="200"
+        step="5"
+        width="10"
+        pathColor="hsl(var(--p))"
+        rangeColor="hsl(var(--s))"
+        handleSize="+20"
+        handleColor="hsl(var(--s))"
+        showTooltip="false"
+        readOnly = "true"
+        class="absolute inset-0 left-1/2 transform -translate-x-1/2 top-1/2 transform -translate-y-1/2"
+      ></round-slider>
       <div
-        class="absolute top-3 bg-base-300 h-9 w-11/12 rounded-full shadow-lg"
+        class="top-aquarium absolute top-3 bg-base-200 h-9 w-11/12 outline outline-base-300 rounded-full shadow-lg drop-shadow-2xl"
         style="left: 50%; transform: translateX(-50%)"
       ></div>
-      <div class="fish">
-        <img src="/src/assets/fish.png" class="absolute bottom-20 w-1/2" style="left: 50%; transform: translateX(-50%)" />
-      </div>
       <div class="absolute bottom-40 left-12 small-fish"></div>
       <div class="absolute bottom-52 left-24 small-fish-2"></div>
+      <div class="fish" :class="{'fish-animation': sessionStarted}" >
+        <img src="/src/assets/fish.png" class="absolute bottom-20 w-3/7" style="left: 50%; transform: translateX(-50%)" />
+      </div>
     </div>
     <div v-if="!sessionStarted">
       <span class="text-7xl">{{ totalMinutes }} : 00</span>
@@ -91,8 +112,12 @@ const abandonSession = () => {
 </script>
 
 <style scoped>
-.aquarium {
+.water {
   background: linear-gradient(to top, hsl(var(--in)) 70%, transparent 70%);
+}
+
+.aquarium {
+  background: linear-gradient(to top, hsl(var(--b3)) 90%, transparent 90%);
 }
 
 .small-fish {
@@ -115,16 +140,44 @@ const abandonSession = () => {
   animation: swim 5s ease-in-out infinite;
 }
 
+.fish-animation {
+  animation: swim-flip 5s ease-in-out infinite;
+}
+
 @keyframes swim {
   0% {
     transform: translateX(0);
   }
   50% {
-    transform: translateX(1rem);
+    transform: translateX(2rem);
   }
   100% {
     transform: translateX(0);
   }
 }
+
+@keyframes swim-flip {
+
+  0% {
+    transform: translateX(0)
+  } 
+  30% {
+    transform: translateX(-4rem)
+  }
+  40% {
+    transform: rotateY(180deg) translateX(4rem)
+  }
+  60% {
+    transform: translateX(4rem) rotateY(180deg)
+  }
+  80% {
+    transform: translateX(4rem)
+  }
+  100% {
+    transform: translateX(0)
+  }
+}
+
+
 
 </style>
