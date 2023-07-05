ff689258082e8fac1a35a536497cec71dff5fc16
SERRAND Coralie <coralie.serrand@telecomnancy.eu>
2023-07-05 20:44:22 +0900

    [+] Add select on add task

---
+++ frontend/src/components/selects/CategorySelect.vue
@@ -0,0 +1,24 @@
+<template>
+    <select class="select select-ghost w-32 max-w-xs">
+      <option disabled selected>Category</option>
+      <option>Yellow</option>
+      <option>Blue</option>
+      <option>Red</option>
+      <option>Green</option>   
+    </select>
+  </template>
+  
+  <style scoped>
+  
+  .select {
+      background-color: transparent;
+      outline: none;
+      border: none;
+  }
+  .select option {
+    background-color: transparent;
+    color: #000;
+  }
+  
+  </style>
+  ```
---
+++ frontend/src/components/selects/DateSelect.vue
@@ -0,0 +1,25 @@
+<template>
+  <select class="select select-ghost w-32 max-w-xs">
+    <option disabled selected>Due date</option>
+    <option>Today</option>
+    <option>Tomorrow</option>
+    <option>In a week</option>    
+  </select>
+</template>
+
+<style scoped>
+
+.select {
+    background-color: transparent;
+    outline: none;
+    border: none;
+}
+.select option {
+  background-color: transparent;
+  color: transparent;
+  outline: none;
+  border: none;
+}
+
+</style>
+```
